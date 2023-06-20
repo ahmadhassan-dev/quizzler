cce2fa2ba401a45a2d388589febe10535ec84dc4
Ahmad hassan <ahmadhassan136@gmail.com>
2023-06-20 18:40:37 +0500

    Working on business logic of quiz app

--- quizzler/ViewController.swift
+++ quizzler/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    let questions = [
+    ["Two + Two is equals to Six?","False"],
+    ["Four + One is equals to Five?","True"],
+    ["One + One is equals to twelve","False"],
+    ["Ten + Ten is equals to Twenty","True"],
+    [
+        "Four + Four is equals to Eight","True"
+    ]
+    ]
+    
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        questionLabel.text = questions[index][0]
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        var userAnswer = sender.currentTitle;
+        var actualAnswer  = questions[index][1]
+    
+        if(index+1>=questions.capacity){
+            return
+        }
+        index = index + 1
+        questionLabel.text = questions[index][0]
+    }
+    
 }
 
