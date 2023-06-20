6c6ea5bd72df5696217a44fc29c29da5ee39ea75
Ahmad hassan <ahmadhassan136@gmail.com>
2023-06-20 18:53:01 +0500

    Implement struct in quiz app

---
+++ quizzler/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Question.swift
+//  quizzler
+//
+//  Created by mac on 20/06/2023.
+//
+
+import Foundation
+
+struct Question{
+    let question: String
+    let value: String
+    
+    init(question: String, value: String) {
+        self.question = question
+        self.value = value
+    }
+}
--- quizzler/ViewController.swift
+++ quizzler/ViewController.swift
@@ -15,34 +15,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var questionLabel: UILabel!
     
     let questions = [
-    ["Two + Two is equals to Six?","False"],
-    ["Four + One is equals to Five?","True"],
-    ["One + One is equals to twelve","False"],
-    ["Ten + Ten is equals to Twenty","True"],
-    [
-        "Four + Four is equals to Eight","True"
-    ]
+        Question(question: "Two + Two is equals to Six?", value: "False"),
+        Question(question: "Four + One is equals to Five?", value: "True"),
+        Question(question: "Ten + Ten is equals to Twenty", value: "True")
     ]
     
     var index = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLabel.text = questions[index][0]
+        questionLabel.text = questions[index].question
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         
         var userAnswer = sender.currentTitle;
-        var actualAnswer  = questions[index][1]
+        var actualAnswer  = questions[index].value
     
         if(index+1>=questions.capacity){
             index = 0
-            questionLabel.text = questions[index][0]
+            questionLabel.text = questions[index].question
             return
         }
         index = index + 1
-        questionLabel.text = questions[index][0]
+        questionLabel.text = questions[index].question
     }
     
 }
